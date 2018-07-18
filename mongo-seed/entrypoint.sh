11618f8acaed74b627da182629c43540d4306e66
rakeshaggarwal1980 <rakesh.aggarwal@evry.com>
2018-07-18 16:40:39 +0530

    added initial structure of app

---
+++ mongo-seed/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR='/seed-data/'
+
+echo "Hello world"
+
+for file in "$DIR"*.json
+
+do
+
+fileBaseName="$(basename $file)"
+
+fileName=$(echo "$fileBaseName" | cut -f 1 -d '.')
+echo $fileName
+mongoimport --host mongodb --db subquip --collection  $fileName --type json --file $file --jsonArray
+
+done
+
