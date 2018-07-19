714516317bde050b1cacc5c54e379ecb9600312f
rakeshaggarwal1980 <rakesh.aggarwal@evry.com>
2018-07-19 16:46:18 +0530

    regular commit

--- mongo-seed/entrypoint.sh
+++ mongo-seed/entrypoint.sh
@@ -1,2 +1,19 @@
-DIR='/seed-data/' 
-echo "Hello world" 
+#!/bin/bash
+
+DIR='/seed-data/'
+
+echo "Hello world"
+
+for file in "$DIR"*.json
+
+do
+
+fileBaseName="$(basename $file)"
+
+fileName=$(echo "$fileBaseName" | cut -f 1 -d '.')
+
+echo $fileName
+
+mongoimport --host mongodb --db subquip --collection  $fileName --type json --file $file --jsonArray
+
+done
